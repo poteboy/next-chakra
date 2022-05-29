1a907514d1b26cbb873a95669a1fc2b545fbad45
poteboy <keitatkins4@gmail.com>
2022-05-29 19:00:34 +0900

    introduce sizing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,24 @@
 import React, { FC, memo } from 'react';
 import { HStack, Container, Flex, Button, Text } from '@chakra-ui/react';
-import { colors } from '@src/styles';
+import { colors, sp, MAX_MOBILE_WIDTH } from '@src/styles';
 import { useWindowSize } from '@src/hooks';
+import styled from 'styled-components';
 
 export const Header: FC = memo(() => {
   return (
     <Flex as="header" bg={colors.White} alignItems="center">
-      <Container maxW={'1200px'} py="12px">
+      <ContentBar maxW={'1200px'} py="12px">
         <HStack justifyContent="space-between" mx="auto">
           <Button>HOME</Button>
           <Text>HELLO</Text>
         </HStack>
-      </Container>
+      </ContentBar>
     </Flex>
   );
 });
+
+const ContentBar = styled(Container)`
+  ${sp`
+    max-width: ${MAX_MOBILE_WIDTH * 0.8}px;
+  `}
+`;
